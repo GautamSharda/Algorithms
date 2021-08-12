cd8f47427b6a65603e1efb0c3b5442ac69157a6f
GautamSharda <57648023+GautamSharda@users.noreply.github.com>
2021-08-11 20:19:37 -0500

    Add files via upload

---
+++ dp/composeRanges.py
@@ -0,0 +1,30 @@
+def composeRanges(nums):
+    
+    if len(nums) == 0:
+        return []
+    
+    initial = nums[0]
+    ranges = []
+    curr = 0
+    
+    while curr < len(nums):
+        
+        if curr == len(nums)-1:
+            end = nums[curr]
+            if initial == end:
+                ranges.append(f"{initial}")
+            else:
+                ranges.append(f"{initial}->{end}")
+            return ranges
+            curr += 1
+                
+        if nums[curr+1] != nums[curr]+1:
+            end = nums[curr]
+            if initial == end:
+                ranges.append(f"{initial}")
+            else:
+                ranges.append(f"{initial}->{end}")
+            initial = nums[curr+1]
+            curr += 1
+        else:
+            curr += 1
